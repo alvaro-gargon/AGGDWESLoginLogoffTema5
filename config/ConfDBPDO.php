7dc5e13ac4181b76134f84436148650f256e2dae
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2025-12-02 12:24:30 +0100

    fix - connection to plesk now works

--- config/ConfDBPDO.php
+++ config/ConfDBPDO.php
@@ -9,10 +9,10 @@
 
     //define('DNS' ,'mysql:host=192.168.1.134;dbname:DBAGGDWESProyectoTema4'); //variable para el entorno en casa
     //define('DNS', 'mysql:host=10.199.9.114;dbname:DBAGGDWESProyectoTema4');
-    //define('DNS', 'mysql:host=localhost;dbname:DBAGGDWESProyectoTema4');
+    //define('DNS', 'mysql:host=localhost;dbname:DBAGGDWESProyectoLoginLogoffTema5');
     define('DNS', 'mysql:host=10.199.11.252;dbname:DBAGGDWESProyectoLoginLogoffTema5');
     //define($DSN, 'mysql:host=10.199.9.114;dbname:DBAGGDWESProyectoLoginLogoffTema5');
     define('USERNAME', 'userAGGDWESProyectoLoginLogoffTema5');
-    define('PASSWORD', 'paso');
+    define('PASSWORD', 'Paso,12345678910');//la contraseña es la de la base de datos de plesk
 
 ?>
